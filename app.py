a99295bfcf97e57ee9d478147cbffb248378c409
Sufian Mushtaq <sufian27@users.noreply.github.com>
2020-03-18 22:35:08 -0400

    updates

--- app.py
+++ app.py
@@ -12,14 +12,14 @@ def index():
     if request.method == 'GET':
         return render_template('index.html')
     
-
 @app.route('/next', methods=['GET', 'POST'])
 def next_page():
     if request.method == 'POST':
         print(request.form['group1'])
         create_table_from_csv(int(request.form['group1']))
         json_object = get_db_data_json(int(request.form['group1']))
-        return render_template('index.html', json_data = json_object) #render next page with passing json object
+        return json_object
+        # return render_template('index.html', json_data = json_object) #render next page with passing json object
     else:
         return 'Invalid Data'
 
