343eda08a3878fb666d9fc2f23f8d9ea846f2f6e
Sufian Mushtaq <sufian27@users.noreply.github.com>
2020-03-25 18:15:49 -0400

    changes to database structure

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 #table that stores id and info of users
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.String, primary_key=True)
 
     actions = db.relationship('User_Action', backref='user', lazy='dynamic')
 
@@ -15,7 +15,7 @@ class User_Action(db.Model):
     row_id = db.Column(db.Integer, primary_key=True)
     time_stamp = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     description = db.Column(db.String)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id')) #relationship
+    user_id = db.Column(db.String, db.ForeignKey('user.id')) #relationship
 
     def __init__(self, description, user_id):
         self.description = description
