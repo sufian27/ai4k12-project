00035835910dee0a1fc6c22940db94b0803ace9c
zhouxf14 <zhouxf14@163.com>
2020-04-04 04:37:23 -0400

    click off the tag

--- static/js/face_browser.js
+++ static/js/face_browser.js
@@ -3,7 +3,8 @@
 	var content = "";
     for (data_id in json_dataset) {
     	var src = "/static/image/user" + user_id + "/example" + example_id + "/" + data_id + ".png";
-    	content += "<span id = 'span" + data_id + "'><img id = 'drag" + data_id +"' src = '" + src + "' width = '35' draggable = 'true' ondragstart = 'drag(event)' >" + "</span>";
+    	content += "<span id = 'span" + data_id + "' ><img id = 'drag" + data_id +"' src = '" + src + "' width = '35' draggable = 'true' ondragstart = 'drag(event)' >" + "</span>";
     }
     document.getElementById("face_browser").innerHTML = content;
+
 });
